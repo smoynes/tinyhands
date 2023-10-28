d92f13a1a1a76a0792a14b34e2d747f8f981b49a
Scott Moynes <smoynes@users.noreply.github.com>
2023-10-27 21:49:09 -0400

    Basic fading

--- fader/build.microbitv2.sh
+++ fader/build.microbitv2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Canonical build for BBC Microbit v2. Run from project root.
-cd $(dirname "$0")
+cd $(dirname "$0")/..
 set -ex
 exec tinygo build -target microbit-v2 -programmer cmsis-dap -size full \
      -o FADER.bin ./fader
