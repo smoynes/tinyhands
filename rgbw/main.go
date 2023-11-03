d70f2e3f2f0fbcafac88d0ceb603eaeadb239acc
Scott Moynes <smoynes@users.noreply.github.com>
2023-11-02 22:30:04 -0400

    RGBW LEDs are not supported (yet?) by tinygo

---
+++ rgbw/main.go
@@ -0,0 +1,64 @@
+// rgbw is an experiment in RBGW driving.
+package main
+
+import (
+	"image/color"
+	"machine"
+	"time"
+
+	"tinygo.org/x/drivers/ws2812"
+)
+
+const tickInterval = 200 * time.Millisecond
+
+var neo machine.Pin = machine.GP0
+var ws ws2812.Device
+var leds []color.RGBA
+var whitescale [16]uint8
+var colorSpace = color.RGBAModel
+
+func init() {
+	for i := 0; i < len(whitescale); i++ {
+		whitescale[i] = uint8(i)
+	}
+	neo.Configure(machine.PinConfig{
+		Mode: machine.PinOutput,
+	})
+	ws = ws2812.New(neo)
+}
+
+func fadeLeds(offset int) {
+	c := color.NRGBA{}
+
+	println(offset, len(whitescale))
+	c.R = whitescale[offset%len(whitescale)]
+	c.A = whitescale[offset%len(whitescale)]
+	led := colorSpace.Convert(c).(color.RGBA)
+	leds = []color.RGBA{led}
+}
+
+func main() {
+	println("RGBW on", machine.Device, machine.CPUFrequency()/1000)
+
+	flip := neo.Get()
+	neo ^= neo
+	neo.Set(flip)
+
+	faderIndex := 0
+
+	for {
+		time.Sleep(tickInterval)
+		//fadeLeds(faderIndex)
+		//ws.WriteColors(leds)
+		println(faderIndex, faderIndex%16)
+		ws.WriteByte(byte(0))
+		ws.WriteByte(byte(0))
+		ws.WriteByte(byte(0))
+		ws.WriteByte(byte(faderIndex % 16))
+
+		neo ^= neo
+		neo.Set(flip)
+
+		faderIndex++
+	}
+}
